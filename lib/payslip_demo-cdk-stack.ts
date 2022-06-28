8fce1ec7d23d5ed1610b8d0e0106949c3d47d752
Charles Huang <hzzgood@gmail.com>
2022-06-28 18:33:45 +1000

    change stack file

--- lib/payslip_demo-cdk-stack.ts
+++ lib/payslip_demo-cdk-stack.ts
@@ -1,19 +1,226 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 
-export class PayslipDemoCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+
+export class PayslipDemoCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'PayslipDemoCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // *** Create a new VPC with single NAT Gateway ***
+     const vpc = new ec2.Vpc(this, 'ecs-cdk-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 1,
+      maxAzs: 3
     });
 
-    const topic = new sns.Topic(this, 'PayslipDemoCdkTopic');
+    const clusterAdmin = new iam.Role(this, 'AdminRole', {
+      assumedBy: new iam.AccountRootPrincipal()
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const cluster = new ecs.Cluster(this, "ecs-cluster", {
+      vpc: vpc
+    });
+
+    const logging = new ecs.AwsLogDriver({
+      streamPrefix: "ecs-logs"
+    });
+
+    const taskRole = new iam.Role(this, `ecs-taskRole-${this.stackName}`, {
+      roleName: `ecs-taskRole-${this.stackName}`,
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com')
+    });
+
+    // *** ECS Contructs ***
+    const executionRolePolicy =  new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+    });
+
+    const taskDef = new ecs.FargateTaskDefinition(this, "ecs-taskdef", {
+      taskRole: taskRole
+    });
+
+    taskDef.addToExecutionRolePolicy(executionRolePolicy);
+
+    // const ecrRepository = new ecr.Repository(this, 'payslip-demo-microservice');
+    const ecrRepo = new ecr.Repository(this, 'EcrRepo');
+
+    const container = taskDef.addContainer('payslip-app', {
+      image: ecs.ContainerImage.fromEcrRepository(ecrRepo),
+      memoryLimitMiB: 256,
+      cpu: 256,
+      logging
+    });
+
+    container.addPortMappings({
+      containerPort: 8080,
+      protocol: ecs.Protocol.TCP
+    });
+
+    const fargateService = new ecsp.ApplicationLoadBalancedFargateService(this, "ecs-service", {
+      cluster: cluster,
+      taskDefinition: taskDef,
+      publicLoadBalancer: true,
+      desiredCount: 1,
+      listenerPort: 80,
+      assignPublicIp: true
+    });
+
+    const scaling = fargateService.service.autoScaleTaskCount({ maxCapacity: 6 });
+    scaling.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 10,
+      scaleInCooldown: cdk.Duration.seconds(60),
+      scaleOutCooldown: cdk.Duration.seconds(60)
+    });
+
+    // *** PIPELINE CONSTRUCTS ***
+    const gitHubSource = codebuild.Source.gitHub({
+      owner: 'hzzgood',
+      repo: 'payslip-demo-microservice',
+      webhook: true, // optional, default: true if `webhookFilteres` were provided, false otherwise
+      webhookFilters: [
+        codebuild.FilterGroup.inEventOf(codebuild.EventAction.PUSH).andBranchIs('main'),
+      ], // optional, by default all pushes and Pull Requests will trigger a build
+    });
+
+    // CODEBUILD - project
+    const project = new codebuild.Project(this, 'MyProject', {
+      projectName: `${this.stackName}`,
+      source: gitHubSource,
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_2,
+        privileged: true
+      },
+      environmentVariables: {
+        'CLUSTER_NAME': {
+          value: `${cluster.clusterName}`
+        },
+        'ECR_REPO_URI': {
+          value: `${ecrRepo.repositoryUri}`
+        }
+      },
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: "0.2",
+        phases: {
+          pre_build: {
+            commands: [
+              'env',
+              'export TAG=${CODEBUILD_RESOLVED_SOURCE_VERSION}'
+            ]
+          },
+          build: {
+            commands: [
+              `docker build -t $ECR_REPO_URI:$TAG .`,
+              '$(aws ecr get-login --no-include-email)',
+              'docker push $ECR_REPO_URI:$TAG'
+            ]
+          },
+          post_build: {
+            commands: [
+              'echo "In Post-Build Stage"',
+              'cd ..',
+              "printf '[{\"name\":\"payslip-app\",\"imageUri\":\"%s\"}]' $ECR_REPO_URI:$TAG > imagedefinitions.json",
+              "pwd; ls -al; cat imagedefinitions.json"
+            ]
+          }
+        },
+        artifacts: {
+          files: [
+            'imagedefinitions.json'
+          ]
+        }
+      })
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    const buildOutput = new codepipeline.Artifact();
+
+    const sourceAction = new codepipeline_actions.GitHubSourceAction({
+      actionName: 'GitHub_Source',
+      owner: 'hzzgood',
+      repo: 'payslip-demo-microservice',
+      branch: 'main',
+      oauthToken: cdk.SecretValue.secretsManager("/my/github/token"),
+      // oauthToken: cdk.SecretValue.plainText('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'),
+      output: sourceOutput
+    });
+
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'CodeBuild',
+      project: project,
+      input: sourceOutput,
+      outputs: [buildOutput], // optional
+    });
+
+    const manualApprovalAction = new codepipeline_actions.ManualApprovalAction({
+      actionName: 'Approve',
+    });
+
+    const deployAction = new codepipeline_actions.EcsDeployAction({
+      actionName: 'DeployAction',
+      service: fargateService.service,
+      imageFile: new codepipeline.ArtifactPath(buildOutput, `imagedefinitions.json`)
+    });
+
+    // *** PIPELINE STAGES ***
+    new codepipeline.Pipeline(this, 'MyECSPipeline', {
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [sourceAction],
+        },
+        {
+          stageName: 'Build',
+          actions: [buildAction],
+        },
+        {
+          stageName: 'Approve',
+          actions: [manualApprovalAction],
+        },
+        {
+          stageName: 'Deploy-to-ECS',
+          actions: [deployAction],
+        }
+      ]
+    });
+
+    ecrRepo.grantPullPush(project.role!)
+    project.addToRolePolicy(new iam.PolicyStatement({
+      actions: [
+        "ecs:DescribeCluster",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:BatchGetImage",
+        "ecr:GetDownloadUrlForLayer"
+        ],
+      resources: [`${cluster.clusterArn}`],
+    }));
+
+    //OUTPUT
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: fargateService.loadBalancer.loadBalancerDnsName });
+
+    // Old
+    // new ecsp.ApplicationLoadBalancedFargateService(this, 'PayslipDemoWebServer', {
+    //   taskImageOptions: {
+    //     image: ecs.ContainerImage.fromRegistry('payslip-demo-microservice'),
+    //   },
+    //   publicLoadBalancer: true
+    // });
   }
 }
--- test/payslip_demo-cdk.test.ts
+++ test/payslip_demo-cdk.test.ts
@@ -3,15 +3,15 @@ import { Template, Match } from 'aws-cdk-lib/assertions';
 import * as PayslipDemoCdk from '../lib/payslip_demo-cdk-stack';
 
 test('SQS Queue and SNS Topic Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new PayslipDemoCdk.PayslipDemoCdkStack(app, 'MyTestStack');
-  // THEN
+  // const app = new cdk.App();
+  // // WHEN
+  // const stack = new PayslipDemoCdk.PayslipDemoCdkStack(app, 'MyTestStack');
+  // // THEN
 
-  const template = Template.fromStack(stack);
+  // const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
+  // template.hasResourceProperties('AWS::SQS::Queue', {
+  //   VisibilityTimeout: 300
+  // });
+  // template.resourceCountIs('AWS::SNS::Topic', 1);
 });
